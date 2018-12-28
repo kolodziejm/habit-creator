f163e3e4aea530cae84a88b13d23f24c41460e92
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-12-28 16:07:42 +0100

    changed browserrouter to hashrouter for correct gh-pages routing, added gh-pages

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename="habit-creator">
+    <HashRouter basename="habit-creator">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
