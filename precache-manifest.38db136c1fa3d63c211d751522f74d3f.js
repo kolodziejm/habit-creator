5815c88d2121375f717982f95885e6b79115a430
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-01-08 11:31:01 +0100

    Updates

---
+++ precache-manifest.38db136c1fa3d63c211d751522f74d3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4957e6a57a663b366de",
+    "url": "/habit-creator/static/js/runtime~main.e4957e6a.js"
+  },
+  {
+    "revision": "4e4f7816157459e9d039",
+    "url": "/habit-creator/static/js/main.4e4f7816.chunk.js"
+  },
+  {
+    "revision": "2309a8c6b6d71309a846",
+    "url": "/habit-creator/static/js/1.2309a8c6.chunk.js"
+  },
+  {
+    "revision": "4e4f7816157459e9d039",
+    "url": "/habit-creator/static/css/main.04e0cbf3.chunk.css"
+  },
+  {
+    "revision": "a0b9314ee654fb3e32bd84d643ff6a12",
+    "url": "/habit-creator/index.html"
+  }
+];
