92ec5bad6a860c14aa481e244bd2e974440a8f7c
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-03-16 21:00:20 +0100

    Ready for redeploy

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create instance, set it to localhost, after api deploy set to the actual domain
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://habit-creator.herokuapp.com/api"
 });
 
 export default instance;
