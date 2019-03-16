35295118f87d33985b4bd3d147dea477841d4cad
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-03-16 21:12:29 +0100

    Updates

---
+++ precache-manifest.56e70e80de273bf495d8d03120a8feff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8db83c122b6fffe388b3efe06090e7d",
+    "url": "/static/media/header-landing.a8db83c1.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "619b9380c757afce0eb8",
+    "url": "/static/js/main.619b9380.chunk.js"
+  },
+  {
+    "revision": "66ebbe3ea65282127c0e",
+    "url": "/static/js/1.66ebbe3e.chunk.js"
+  },
+  {
+    "revision": "619b9380c757afce0eb8",
+    "url": "/static/css/main.9ad893a6.chunk.css"
+  },
+  {
+    "revision": "eb18a8cc66766c61cf61d6d5eb3547bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/habit-creator/precache-manifest.c65eee0d55d8423272258533040564d6.js"
+  "/precache-manifest.56e70e80de273bf495d8d03120a8feff.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/habit-creator/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
